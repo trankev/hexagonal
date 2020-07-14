adc79152ade0baf191a52f819d507e2e008869da
Kevin Tran <kevin_tran@live.fr>
2020-07-14 14:18:21 +0200

    Add update method to resources

--- src/hexagonal/transport_layers/rest_api/resources.py
+++ src/hexagonal/transport_layers/rest_api/resources.py
@@ -18,6 +18,7 @@ class Resource:
     list_interactor: typing.Optional[interactors.BRBRInteractor] = None
     create_interactor: typing.Optional[interactors.BRBRInteractor] = None
     retrieve_interactor: typing.Optional[interactors.BRBRInteractor] = None
+    update_interactor: typing.Optional[interactors.BRBRInteractor] = None
 
     def iterate_routes(self) -> typing.Iterator[Route]:
         if self.list_interactor:
@@ -39,4 +40,11 @@ class Resource:
                 interactor=self.retrieve_interactor,
                 success_code=200,
                 methods=["GET"],
+            )
+        if self.update_interactor:
+            yield Route(
+                path=f"/{self.name}/{{item_id}}",
+                interactor=self.update_interactor,
+                success_code=200,
+                methods=["PUT"],
             )
