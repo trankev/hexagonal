9f1201119ed43d4b7d2f7d5604567778abcd20b9
Kevin Tran <kevin_tran@live.fr>
2020-07-23 15:19:43 +0200

    Test resource availability

--- tests/integration/transport_layers/rest_api/starlette/test_resource.py
+++ tests/integration/transport_layers/rest_api/starlette/test_resource.py
@@ -16,6 +16,9 @@ def rest_api_fixture(mock_service: services.ABBService) -> starlette.application
             resources.Resource(
                 name="resources",
                 list_service=mock_service,
+                retrieve_service=mock_service,
+                create_service=mock_service,
+                update_service=mock_service,
             ),
         ],
     )
@@ -28,12 +31,24 @@ def test_client_fixture(rest_api: api.API) -> starlette.testclient.TestClient:
     return starlette.testclient.TestClient(rest_api)
 
 
-def test_nominal(
+@pytest.mark.parametrize(
+    ("path", "method", "expected_status"),
+    (
+        ("/resources", "GET", 200),
+        ("/resources", "POST", 201),
+        ("/resources/some_id", "GET", 200),
+        ("/resources/some_id", "PUT", 200),
+    ),
+)
+def test_availability(
     test_client: starlette.testclient.TestClient,
     mock_service: utils.MockService,
+    path: str,
+    method: str,
+    expected_status: int,
 ) -> None:
-    response = test_client.get("/resources")
-    assert response.status_code == 200
+    response = test_client.request(method, path)
+    assert response.status_code == expected_status
     expected = mock_service.response().json()
 
     assert response.text == expected
